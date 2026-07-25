48f1048fb1e4337639388e106ffeb99ea009cf19
Linux-Plebeian <linuxnoob373@gmail.com>
2026-07-25 11:34:29 -0400

    updated means of reading training data

--- main.py
+++ main.py
@@ -5,40 +5,28 @@ import trainer as tr
 def dingus(input_layer, weights, bias):
     return tr.sigmoid(np.dot(input_layer, weights) + bias)
 
-input_faces = np.array([[0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],# happy
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],# sad
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-                [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0]])
-
-desired_outputs = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).T
+categories = 2
+elements = 9
+input_faces = ita.read_training_data(categories,elements)
+list_desired_outputs = []
+for i in range(0,categories*elements):
+    if i<elements:
+        list_desired_outputs.append(1)
+    else:
+        list_desired_outputs.append(0)
+desired_outputs = np.array([list_desired_outputs]).T
 
 
 training_data = tr.train(input_faces, desired_outputs)
 weights = training_data[0]
 bias = training_data[1]
-
-print(weights)
-
+outputs = training_data[2]
 while True:
-    path = input("Enter picture path: ")
+    path = input("Enter image filename: ")
     input_image = ita.test(path)
     print(input_image)
     if dingus(input_image, weights, bias)  > .5:
-        print("Happy :)")
+        print(":]")
     else:
-        print("Sad :(")
+        print(":[")
     
--- trainer.py
+++ trainer.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-
+# Constants
+image_lw = 16
 
 # Activation Function
 def sigmoid(x):
@@ -12,32 +12,26 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 def train(training_inputs, training_outputs):
-
     # Initialize Parameters
     np.random.seed(1)
-    weights = 2 * np.random.random((16, 1)) - 1
+    weights = 2 * np.random.random((image_lw, 1)) - 1
     bias = 0
-
     #print("Initial Weights:\n", weights)
-
     learning_rate = 1
-
-    for epoch in range(1000):
+    for train_loop in range(1000):
         # Forward Pass
         input_layer = training_inputs
         outputs = sigmoid(np.dot(input_layer, weights) + bias)
-        
         error = training_outputs - outputs
-        
         # Backpropagation
         adjustments = error * sigmoid_derivative(outputs)
-        
         # Update Parameters
         weights += np.dot(input_layer.T, adjustments) * learning_rate
         bias += np.sum(adjustments) * learning_rate
-
+        if train_loop % 1000 == 1:
+            print(np.round(outputs, decimals=2))
     #print("\nWeights After Training:\n", weights)
     #print("\nOutputs After Training:\n", np.round(outputs,1))
-    return weights, bias
+    return weights, bias, outputs
 
     
