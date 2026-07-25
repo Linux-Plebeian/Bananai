8ff38e38633c4bc8901661177e583b752e290168
Linux-Plebeian <linuxnoob373@gmail.com>
2026-07-25 11:34:29 -0400

    updated means of reading training data

--- imgtoarray.py
+++ imgtoarray.py
@@ -1,17 +1,20 @@
 import numpy as np
 from PIL import Image
 
-def export_all(categories, images_per_category): # Name images <category>(<item>) starting with 1 for categories and 0 for item no. -> ex. 2(1)
+def read_training_data(categories, images_per_category): # Name images <category>(<item>) starting with 1 for categories and 0 for item no. -> ex. 2(1)
+    data = [[]]
     for i in range(1,categories+1):
-        for j in range(0,images_per_category+1):
-            img = Image.open(f"training_data/{i}({j}).png")
+        for j in range(0,images_per_category):
+            img = Image.open(f"training_data/faces/{i}({j}).png")
 
             img_bin = img.convert("1")
-            matrix = np.array(img_bin).astype(int)
-            final_output = matrix.flatten()
-
-            #print(matrix)
-            print(final_output.tolist())
+            img_arr2d = np.array(img_bin).astype(int)
+            img_list = img_arr2d.flatten().tolist()
+            data.append(img_list) 
+    del data[0] 
+    r = np.array(data)
+    print(r)
+    return r
 def test(image):
     img = Image.open(f"images/{image}")
 
