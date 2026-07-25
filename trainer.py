e25147440a447c46c56454a715cd92abab733845
Linux-Plebeian <linuxnoob373@gmail.com>
2026-07-24 20:41:43 -0400

    Made a neural network capable of recognizing 4x4 faces (Happy or Sad)

---
+++ trainer.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+
+# Activation Function
+def sigmoid(x):
+    r = 1 / (1 + np.exp(-x))
+    #print (r)
+    return r
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+def train(training_inputs, training_outputs):
+
+    # Initialize Parameters
+    np.random.seed(1)
+    weights = 2 * np.random.random((16, 1)) - 1
+    bias = 0
+
+    #print("Initial Weights:\n", weights)
+
+    learning_rate = 1
+
+    for epoch in range(1000):
+        # Forward Pass
+        input_layer = training_inputs
+        outputs = sigmoid(np.dot(input_layer, weights) + bias)
+        
+        error = training_outputs - outputs
+        
+        # Backpropagation
+        adjustments = error * sigmoid_derivative(outputs)
+        
+        # Update Parameters
+        weights += np.dot(input_layer.T, adjustments) * learning_rate
+        bias += np.sum(adjustments) * learning_rate
+
+    #print("\nWeights After Training:\n", weights)
+    #print("\nOutputs After Training:\n", np.round(outputs,1))
+    return weights, bias
+
+    
