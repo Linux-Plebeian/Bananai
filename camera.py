abc42207598bf7f50f9bc50d6dbf5d8d68ad49ad
Linux-Plebeian <linuxnoob373@gmail.com>
2026-07-30 21:46:28 -0400

    More layers, more bananas

--- camera.py
+++ camera.py
@@ -1,18 +1,21 @@
 import cv2
 from PIL import Image
 def main():
-    vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture(1)
 
     while True:
-        ret, frame = vid.read()
-        cv2.imshow('Camera Feed', frame)
-        cv2.imwrite('images/banana.png', frame)
+        for i in range(1,4):
+            ret, frame = vid.read()
+            cv2.imshow('Camera Feed', frame)
+            cv2.imwrite(f"images/banana{i}.png", frame)
+            img = Image.open(f"images/banana{i}.png")
+            img = img.resize((28, 28))
+            img.save(f"images/banana{i}.png")
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+            
 
-    img = Image.open('images/banana.png')
-    img = img.resize((28, 28))
-    img.save('images/banana.png')
+    
 
     vid.release()
     cv2.destroyAllWindows()
