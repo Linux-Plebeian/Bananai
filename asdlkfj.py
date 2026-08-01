9eb1d2f4411ad29939f911e0f5a8ff8ddf3d3096
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-01 14:41:28 -0400

    More layers, more bananas

--- asdlkfj.py
+++ asdlkfj.py
@@ -1,3 +1,3 @@
-print("B A N A N A Z")
+
 def nuts():
     return "B A N A N A"
--- imgtoarray.py
+++ imgtoarray.py
@@ -17,10 +17,10 @@ def read_training_data(elements): # Name images <category>(<item>) starting with
     data = [[]]
     for i in range(0,elements):
         img = Image.open(f"training_images/{i}.png")
-        img_grayscale = img.convert('L')
-        img_bin = img_grayscale.convert("1")
-        img_arr2d = np.array(img_bin)
-        img_list = img_arr2d.flatten().tolist()
+        img_gs = img.convert('L')
+        #img_bin = img_gs.convert("1")
+        img_arr2d = np.array(img_gs)
+        img_list = (img_arr2d/255).flatten().tolist()
         data.append(img_list) 
     del data[0] 
     r = np.array(data)
