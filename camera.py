04526a16771c5fa0c0ea47dbebf6590f32b07660
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 11:37:28 -0400

    oled test

--- camera.py
+++ camera.py
@@ -3,18 +3,19 @@ from PIL import Image
 import oled
 def main():
     vid = cv2.VideoCapture(0, cv2.CAP_V4L2)
-
+    test = 0
     while True:
         for i in range(1,4):
             ret, frame = vid.read()
             #cv2.imshow('Camera Feed', frame)
             cv2.imwrite(f"images/banana{i}.png", frame)
             img = Image.open(f"images/banana{i}.png")
-            img = img.resize((28, 28))
-            img.save(f"images/banana{i}.png")
-            img = Image.open(f"images/banana{i}.png")
             img = img.resize((128, 64))
             img.save(f"camera/banana{i}.png")
+            img = img.resize((28, 28))
+            img.save(f"images/banana{i}.png")
+            print("frame",test)
+            test+=1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
             
