4ec975a6642bb04215031841f86e55dfa84189ef
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 11:20:00 -0400

    oled test 6

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 sudo apt install python3
+sudo apt install python3-pip
 sudo apt install python3-opencv
+sudo apt install fswebcam
 sudo apt install python3-pil
 sudo apt install python3-numpy
-sudo apt-get install python-smbus
+sudo apt-get install python3-smbus
 sudo apt-get install i2c-tools
 
 pip install adafruit-circuitpython-ssd1306 adafruit-blinka --break-system-packages
