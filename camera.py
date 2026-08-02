1af49d93a51fe5b3a97ff470955f92e015fde7e5
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 08:47:24 -0400

    oled test 5

--- camera.py
+++ camera.py
@@ -1,16 +1,20 @@
 import cv2
 from PIL import Image
+import oled
 def main():
     vid = cv2.VideoCapture(1)
 
     while True:
         for i in range(1,4):
             ret, frame = vid.read()
-            cv2.imshow('Camera Feed', frame)
+            #cv2.imshow('Camera Feed', frame)
             cv2.imwrite(f"images/banana{i}.png", frame)
             img = Image.open(f"images/banana{i}.png")
             img = img.resize((28, 28))
             img.save(f"images/banana{i}.png")
+            img = Image.open(f"images/banana{i}.png")
+            img = img.resize((128, 64))
+            img.save(f"camera/banana{i}.png")
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
             
--- oled.py
+++ oled.py
@@ -5,7 +5,15 @@ import adafruit_ssd1306
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
-oled.fill(0)
-oled.show()
-oled.text("Hello, BANANANANANANAN", 0, 0, 1)
-oled.show()
+def draw_sprite(x, y, l, w, sprite_bin);
+    for i in range(0,l):
+        for j in range(0,w):
+            if sprite_bin[i + j*w] > 0.5
+                oled.pixel(i,j,1)
+    oled.show()
+            
+def test():
+    oled.fill(0)
+    oled.show()
+    oled.text("Hello, BANANANANANANAN", 0, 0, 1)
+    oled.show()
