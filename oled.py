ec248c7d724e9c0d20a483922b77515cb3e61b76
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 14:24:11 -0400

    oled test

--- oled.py
+++ oled.py
@@ -5,14 +5,13 @@ import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
-
+oled.text("BananAI v0.2", 0, 0, 1)
 def draw_sprite(x, y, l, h, sprite_bin):
     for i in range(0,l):
         for j in range(0,h):
-            if sprite_bin[i + j*l] > 0.5:
+            if sprite_bin[i + j*l] = 1:
                 oled.pixel(i,j,1)
-                oled.text(0,0,"BananAI v0.2", 1)
-                oled.show()
+    oled.show()
 
 def clear():
     oled.fill(0)           
