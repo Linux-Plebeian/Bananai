fb3e9a3de8056c511975353e59f566c565001e23
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 20:47:10 -0400

    added rpi camera support

--- rpi/install.sh
+++ rpi/install.sh
@@ -6,6 +6,7 @@ sudo apt install python3-pil
 sudo apt install python3-numpy
 sudo apt-get install python3-smbus
 sudo apt-get install i2c-tools
+sudo apt install python3-picamera2
 
 pip3 install board --break-system-packages
 pip3 install keyboard --break-system-packages
