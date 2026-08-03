4a7eaabe76f325d164b0db8e74fe9176f04f2644
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 21:15:09 -0400

    camera update

--- rpi/camera.py
+++ rpi/camera.py
@@ -12,19 +12,22 @@ breakage = 0
 picam2 = Picamera2() 
 config = picam2.create_still_configuration() 
 picam2.configure(config) 
+
 picam2.start() 
+picam2.resolution = (128, 64)
 def deez():
     print("Processing...")
     breakage = 1
     
 def main():
-    vid = picam2.capture_array()
+    
     test = 0
     breakage = 0
     while True:
         for i in range(1,4):
             #ret, frame = vid.read()
             #cv2.imshow('Camera Feed', frame)
+            vid = picam2.capture_array()
             cv2.imwrite(f"images/banana{i}.png", vid)
             img = Image.open(f"images/banana{i}.png")
             img = img.resize((128, 64))
