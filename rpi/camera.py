02ff57265d14734ec8fd20cfc7eb2b9066abc6e1
Linux-Plebeian <linuxnoob373@gmail.com>
2026-08-02 20:47:10 -0400

    added rpi camera support

--- rpi/camera.py
+++ rpi/camera.py
@@ -3,22 +3,29 @@ from PIL import Image
 import oled
 import imgtoarray as ita
 import keyboard
+from picamera2 import Picamera2 
+import time 
 
 breakage = 0
 
+
+picam2 = Picamera2() 
+config = picam2.create_still_configuration() 
+picam2.configure(config) 
+picam2.start() 
 def deez():
     print("Processing...")
     breakage = 1
     
 def main():
-    vid = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    vid = picam2.capture_array()
     test = 0
     breakage = 0
     while True:
         for i in range(1,4):
-            ret, frame = vid.read()
+            #ret, frame = vid.read()
             #cv2.imshow('Camera Feed', frame)
-            cv2.imwrite(f"images/banana{i}.png", frame)
+            cv2.imwrite(f"images/banana{i}.png", vid)
             img = Image.open(f"images/banana{i}.png")
             img = img.resize((128, 64))
             img.save(f"camera/banana{i}.png")
--- rpi/imgtoarray.py
+++ rpi/imgtoarray.py
@@ -2,30 +2,6 @@ import numpy as np
 from PIL import Image
 import os
 
-def name_data(name, name2):
-    for i in range(0,10):
-        os.rename(f"./training_images/{name}_0"+str(i)+".png", "./training_images/"+str(i)+".png")
-    for i in range(10,72):
-        os.rename(f"./training_images/{name}_"+str(i)+".png", "./training_images/"+str(i)+".png")
-    for i in range(0,10):
-        os.rename(f"./training_images/{name2}_0"+str(i)+".png", "./training_images/"+str(i+72)+".png")
-    for i in range(10,72):
-        os.rename(f"./training_images/{name2}_"+str(i)+".png", "./training_images/"+str(i+72)+".png")
-
-
-def read_training_data(elements): 
-    data = []
-    for i in range(0,elements):
-        img = Image.open(f"training_images/{i}.png")
-        img_gs = img.convert('L')
-        #img_bin = img_gs.convert("1")
-        img_arr2d = np.array(img_gs)
-        img_list = (img_arr2d/255).flatten().tolist()
-        data.append(img_list) 
-    r = np.array(data)
-    print(r)
-    return r
-    
 def convert(image):
     img = Image.open(f"{image}")
     img_gs = img.convert('L')
